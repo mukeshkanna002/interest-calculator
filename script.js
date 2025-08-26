ba9dd837fcd08ffa748af49cc0710f2bfb2bbe23
mukeshkanna002 <mukeshkanna2002@gmail.com>
2025-08-26 13:49:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -27,11 +27,9 @@ function calculateInterest() {
   }
 
   let totalMonths = months + extraInterestMonths;
-  let Total =principal * rate * totalMonths + interest;
   let interest = principal * rate * totalMonths;
 
   document.getElementById("result").innerHTML =
     `📅 Duration: ${months} month(s) ${days} day(s)<br>` +
-    `📅 Total: ${Total} <br>` +
     `💰 Interest @ ${rate * 100}%: <b>${interest.toFixed(2)}</b>`;
 }
